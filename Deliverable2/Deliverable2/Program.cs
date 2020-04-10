21ffdc4fca6c3124d32c98ffd5c900f4caef0907
kgraham21 <55333293+kgraham21@users.noreply.github.com>
2020-04-10 10:48:35 -0400

    First commit for Deliverable2

---
+++ Deliverable2/Deliverable2/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace Deliverable2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your message? ");
+            string input = Console.ReadLine();
+            string inputUpper = input.ToUpper();
+
+
+
+            Console.WriteLine("Your encoded message is: ");
+            foreach (char c in inputUpper)
+
+                
+                Console.Write(((int)c - 64) + "-");
+
+            static long ASCIIWordSum(string inputUpper, long[] sumArr)
+            {
+                int l = inputUpper.Length;
+                int pos = 0;
+                long sum = 0;
+                long bigSum = 0;
+                for (int i = 0; i < l; i++)
+                {
+
+                    
+                    if (inputUpper[i] == ' ')
+                    {
+                        bigSum += sum;
+                        sumArr[pos++] = sum;
+                        sum = 0;
+                    }
+                    else
+
+                        
+                        sum += inputUpper[i];
+                }
+
+                 
+                sumArr[pos] = sum;
+                bigSum += sum;
+                return bigSum;
+            }
+
+            
+                
+                int ctr = 0;
+                for (int i = 0; i < inputUpper.Length; i++)
+                    if (inputUpper[i] == ' ')
+                        ctr++;
+
+                long[] sumArr = new long[ctr + 1];
+
+                
+                long sum = ASCIIWordSum(inputUpper, sumArr);
+
+                
+
+                Console.WriteLine();
+                
+            int checksum = (int)sum;
+            Console.Write("Checksum Value " + checksum);
+        }
+        }
+
+        
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
